f54974e7f36dc9180c7b92feb698ae8792f03737
reytdn <reynaldotuden88@gmail.com>
2026-01-29 14:16:45 +0800

    task

--- BORROWTRANSACT.java
+++ BORROWTRANSACT.java
@@ -8,6 +8,7 @@ public class BORROWTRANSACT {
         System.out.println("Book Genre: " + books.getgenre());
         System.out.println("Book Title: " + books.gettitle());
         System.out.println("Book Borrowed Time: " + books.getborrowtime());
+        System.out.println("hi");
         
     } 
 }
